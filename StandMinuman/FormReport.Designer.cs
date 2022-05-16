159175cbf553754eb452f4e03dc0438353427bbd
AzhureRaven <abrahamarthurfendy@gmail.com>
2022-05-16 21:51:17 +0700

    Laporan Topping

--- StandMinuman/FormReport.Designer.cs
+++ StandMinuman/FormReport.Designer.cs
@@ -29,95 +29,167 @@ namespace StandMinuman
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-			this.sellerpenjualanTertinggiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.sellerpenjualanTerendahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.minumanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.stokMinumanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.toppingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.kembaliKeMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-			this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
-			this.menuStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// menuStrip1
-			// 
-			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sellerpenjualanTertinggiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sellerpenjualanTerendahToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.minumanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.stokMinumanToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toppingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.kembaliKeMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
+            this.dateTimePickerAwal = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePickerAkhir = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonTampil = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.sellerpenjualanTertinggiToolStripMenuItem,
             this.sellerpenjualanTerendahToolStripMenuItem,
             this.minumanToolStripMenuItem,
             this.stokMinumanToolStripMenuItem,
             this.toppingToolStripMenuItem,
             this.kembaliKeMenuToolStripMenuItem});
-			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-			this.menuStrip1.Name = "menuStrip1";
-			this.menuStrip1.Size = new System.Drawing.Size(1011, 24);
-			this.menuStrip1.TabIndex = 0;
-			this.menuStrip1.Text = "menuStrip1";
-			// 
-			// sellerpenjualanTertinggiToolStripMenuItem
-			// 
-			this.sellerpenjualanTertinggiToolStripMenuItem.Name = "sellerpenjualanTertinggiToolStripMenuItem";
-			this.sellerpenjualanTertinggiToolStripMenuItem.Size = new System.Drawing.Size(158, 20);
-			this.sellerpenjualanTertinggiToolStripMenuItem.Text = "Seller (penjualan tertinggi)";
-			// 
-			// sellerpenjualanTerendahToolStripMenuItem
-			// 
-			this.sellerpenjualanTerendahToolStripMenuItem.Name = "sellerpenjualanTerendahToolStripMenuItem";
-			this.sellerpenjualanTerendahToolStripMenuItem.Size = new System.Drawing.Size(160, 20);
-			this.sellerpenjualanTerendahToolStripMenuItem.Text = "Seller (penjualan terendah)";
-			// 
-			// minumanToolStripMenuItem
-			// 
-			this.minumanToolStripMenuItem.Name = "minumanToolStripMenuItem";
-			this.minumanToolStripMenuItem.Size = new System.Drawing.Size(126, 20);
-			this.minumanToolStripMenuItem.Text = "Penjualan Minuman";
-			// 
-			// stokMinumanToolStripMenuItem
-			// 
-			this.stokMinumanToolStripMenuItem.Name = "stokMinumanToolStripMenuItem";
-			this.stokMinumanToolStripMenuItem.Size = new System.Drawing.Size(97, 20);
-			this.stokMinumanToolStripMenuItem.Text = "Stok Minuman";
-			// 
-			// toppingToolStripMenuItem
-			// 
-			this.toppingToolStripMenuItem.Name = "toppingToolStripMenuItem";
-			this.toppingToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
-			this.toppingToolStripMenuItem.Text = "Topping";
-			// 
-			// kembaliKeMenuToolStripMenuItem
-			// 
-			this.kembaliKeMenuToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
-			this.kembaliKeMenuToolStripMenuItem.Name = "kembaliKeMenuToolStripMenuItem";
-			this.kembaliKeMenuToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
-			this.kembaliKeMenuToolStripMenuItem.Text = "Kembali ke Menu";
-			this.kembaliKeMenuToolStripMenuItem.Click += new System.EventHandler(this.kembaliKeMenuToolStripMenuItem_Click);
-			// 
-			// crystalReportViewer1
-			// 
-			this.crystalReportViewer1.ActiveViewIndex = -1;
-			this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-			this.crystalReportViewer1.Location = new System.Drawing.Point(0, 56);
-			this.crystalReportViewer1.Name = "crystalReportViewer1";
-			this.crystalReportViewer1.Size = new System.Drawing.Size(1011, 539);
-			this.crystalReportViewer1.TabIndex = 1;
-			// 
-			// FormReport
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1011, 589);
-			this.Controls.Add(this.crystalReportViewer1);
-			this.Controls.Add(this.menuStrip1);
-			this.MainMenuStrip = this.menuStrip1;
-			this.Name = "FormReport";
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-			this.Text = "FormReport";
-			this.menuStrip1.ResumeLayout(false);
-			this.menuStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1516, 35);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sellerpenjualanTertinggiToolStripMenuItem
+            // 
+            this.sellerpenjualanTertinggiToolStripMenuItem.Name = "sellerpenjualanTertinggiToolStripMenuItem";
+            this.sellerpenjualanTertinggiToolStripMenuItem.Size = new System.Drawing.Size(233, 29);
+            this.sellerpenjualanTertinggiToolStripMenuItem.Text = "Seller (penjualan tertinggi)";
+            // 
+            // sellerpenjualanTerendahToolStripMenuItem
+            // 
+            this.sellerpenjualanTerendahToolStripMenuItem.Name = "sellerpenjualanTerendahToolStripMenuItem";
+            this.sellerpenjualanTerendahToolStripMenuItem.Size = new System.Drawing.Size(236, 29);
+            this.sellerpenjualanTerendahToolStripMenuItem.Text = "Seller (penjualan terendah)";
+            // 
+            // minumanToolStripMenuItem
+            // 
+            this.minumanToolStripMenuItem.Name = "minumanToolStripMenuItem";
+            this.minumanToolStripMenuItem.Size = new System.Drawing.Size(182, 29);
+            this.minumanToolStripMenuItem.Text = "Penjualan Minuman";
+            // 
+            // stokMinumanToolStripMenuItem
+            // 
+            this.stokMinumanToolStripMenuItem.Name = "stokMinumanToolStripMenuItem";
+            this.stokMinumanToolStripMenuItem.Size = new System.Drawing.Size(143, 29);
+            this.stokMinumanToolStripMenuItem.Text = "Stok Minuman";
+            // 
+            // toppingToolStripMenuItem
+            // 
+            this.toppingToolStripMenuItem.Name = "toppingToolStripMenuItem";
+            this.toppingToolStripMenuItem.Size = new System.Drawing.Size(93, 29);
+            this.toppingToolStripMenuItem.Text = "Topping";
+            this.toppingToolStripMenuItem.Click += new System.EventHandler(this.toppingToolStripMenuItem_Click);
+            // 
+            // kembaliKeMenuToolStripMenuItem
+            // 
+            this.kembaliKeMenuToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.kembaliKeMenuToolStripMenuItem.Name = "kembaliKeMenuToolStripMenuItem";
+            this.kembaliKeMenuToolStripMenuItem.Size = new System.Drawing.Size(164, 29);
+            this.kembaliKeMenuToolStripMenuItem.Text = "Kembali ke Menu";
+            this.kembaliKeMenuToolStripMenuItem.Click += new System.EventHandler(this.kembaliKeMenuToolStripMenuItem_Click);
+            // 
+            // crystalReportViewer1
+            // 
+            this.crystalReportViewer1.ActiveViewIndex = -1;
+            this.crystalReportViewer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 86);
+            this.crystalReportViewer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.crystalReportViewer1.Name = "crystalReportViewer1";
+            this.crystalReportViewer1.Size = new System.Drawing.Size(1516, 828);
+            this.crystalReportViewer1.TabIndex = 1;
+            this.crystalReportViewer1.ToolPanelWidth = 300;
+            // 
+            // dateTimePickerAwal
+            // 
+            this.dateTimePickerAwal.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePickerAwal.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dateTimePickerAwal.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerAwal.Location = new System.Drawing.Point(157, 48);
+            this.dateTimePickerAwal.Name = "dateTimePickerAwal";
+            this.dateTimePickerAwal.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePickerAwal.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(8, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Tanggal Awal :";
+            // 
+            // dateTimePickerAkhir
+            // 
+            this.dateTimePickerAkhir.CalendarFont = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.dateTimePickerAkhir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dateTimePickerAkhir.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePickerAkhir.Location = new System.Drawing.Point(524, 48);
+            this.dateTimePickerAkhir.Name = "dateTimePickerAkhir";
+            this.dateTimePickerAkhir.Size = new System.Drawing.Size(200, 30);
+            this.dateTimePickerAkhir.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(373, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Tanggal Akhir :";
+            // 
+            // buttonTampil
+            // 
+            this.buttonTampil.AutoSize = true;
+            this.buttonTampil.Enabled = false;
+            this.buttonTampil.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.buttonTampil.Location = new System.Drawing.Point(1410, 42);
+            this.buttonTampil.Name = "buttonTampil";
+            this.buttonTampil.Size = new System.Drawing.Size(94, 36);
+            this.buttonTampil.TabIndex = 6;
+            this.buttonTampil.Text = "Tampil";
+            this.buttonTampil.UseVisualStyleBackColor = true;
+            this.buttonTampil.Click += new System.EventHandler(this.buttonTampil_Click);
+            // 
+            // FormReport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1516, 906);
+            this.Controls.Add(this.buttonTampil);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dateTimePickerAkhir);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePickerAwal);
+            this.Controls.Add(this.crystalReportViewer1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "FormReport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormReport";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -131,5 +203,10 @@ namespace StandMinuman
 		private System.Windows.Forms.ToolStripMenuItem toppingToolStripMenuItem;
 		private System.Windows.Forms.ToolStripMenuItem kembaliKeMenuToolStripMenuItem;
 		private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
-	}
+        private System.Windows.Forms.DateTimePicker dateTimePickerAwal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerAkhir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonTampil;
+    }
 }
--- StandMinuman/FormReport.cs
+++ StandMinuman/FormReport.cs
@@ -12,6 +12,7 @@ namespace StandMinuman
 {
 	public partial class FormReport : Form
 	{
+		int report = -1;
 		public FormReport()
 		{
 			InitializeComponent();
@@ -21,5 +22,69 @@ namespace StandMinuman
 		{
 			this.Close();
 		}
-	}
+
+        private void buttonTampil_Click(object sender, EventArgs e)
+        {
+            if (report != -1)
+            {
+                if(report == 1)
+                {
+                    //report seller tertinggi
+                }
+                else if(report == 2)
+                {
+                    //report seller terendah
+                }
+                else if(report == 3)
+                {
+                    //report minuman
+                }
+                else if(report == 4)
+                {
+                    //report stok minuman
+                }
+                else
+                {
+                    reportTopping();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Test");
+            }
+        }
+
+        public void reportTopping()
+        {
+            if (dateTimePickerAwal.Value < dateTimePickerAkhir.Value)
+            {
+                CrystalReportBanyakTopping rpt = new CrystalReportBanyakTopping();
+                rpt.SetParameterValue("TanggalAwal", dateTimePickerAwal.Value);
+                rpt.SetParameterValue("TanggalAkhir", dateTimePickerAkhir.Value);
+                crystalReportViewer1.ReportSource = rpt;
+            }
+            else
+            {
+                MessageBox.Show("Tanggal akhir harus lebih besar dari tanggal awal!");
+            }
+        }
+
+        private void toppingToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            report = 5;
+            buttonTampil.Enabled = true;
+            resetBold();
+            toppingToolStripMenuItem.Font = new Font(toppingToolStripMenuItem.Font, FontStyle.Bold);
+        }
+
+        public void resetBold()
+        {
+            sellerpenjualanTertinggiToolStripMenuItem.Font = new Font(sellerpenjualanTertinggiToolStripMenuItem.Font, FontStyle.Regular);
+            sellerpenjualanTerendahToolStripMenuItem.Font = new Font(sellerpenjualanTerendahToolStripMenuItem.Font, FontStyle.Regular);
+            minumanToolStripMenuItem.Font = new Font(minumanToolStripMenuItem.Font, FontStyle.Regular);
+            stokMinumanToolStripMenuItem.Font = new Font(stokMinumanToolStripMenuItem.Font, FontStyle.Regular);
+            toppingToolStripMenuItem.Font = new Font(toppingToolStripMenuItem.Font, FontStyle.Regular);
+            
+        }
+    }
 }
