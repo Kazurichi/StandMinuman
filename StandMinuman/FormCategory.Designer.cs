66e6a0e35472265fd78af9f203aaa0aaf5bd3631
Kevin Jonathan <kevinjonathan@istts.onmicrosoft.com>
2022-05-23 20:23:45 +0700

    AddFormCategory

---
+++ StandMinuman/FormCategory.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace StandMinuman
+{
+	partial class FormCategory
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.buttonClearSearch = new System.Windows.Forms.Button();
+			this.comboBoxAktif = new System.Windows.Forms.ComboBox();
+			this.textBoxSearch = new System.Windows.Forms.TextBox();
+			this.label6 = new System.Windows.Forms.Label();
+			this.buttonClear = new System.Windows.Forms.Button();
+			this.buttonDelete = new System.Windows.Forms.Button();
+			this.buttonUpdate = new System.Windows.Forms.Button();
+			this.btnInsert = new System.Windows.Forms.Button();
+			this.textBoxUsername = new System.Windows.Forms.TextBox();
+			this.textBoxId = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label1 = new System.Windows.Forms.Label();
+			this.dataGridViewUser = new System.Windows.Forms.DataGridView();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.kembaliKeMenuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.masterTransaksiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).BeginInit();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// buttonClearSearch
+			// 
+			this.buttonClearSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.buttonClearSearch.Location = new System.Drawing.Point(557, 472);
+			this.buttonClearSearch.Margin = new System.Windows.Forms.Padding(4);
+			this.buttonClearSearch.Name = "buttonClearSearch";
+			this.buttonClearSearch.Size = new System.Drawing.Size(116, 36);
+			this.buttonClearSearch.TabIndex = 66;
+			this.buttonClearSearch.Text = "Clear Search";
+			this.buttonClearSearch.UseVisualStyleBackColor = true;
+			// 
+			// comboBoxAktif
+			// 
+			this.comboBoxAktif.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.comboBoxAktif.FormattingEnabled = true;
+			this.comboBoxAktif.Items.AddRange(new object[] {
+            "Semua",
+            "Aktif",
+            "Tidak Aktif"});
+			this.comboBoxAktif.Location = new System.Drawing.Point(369, 478);
+			this.comboBoxAktif.Margin = new System.Windows.Forms.Padding(4);
+			this.comboBoxAktif.Name = "comboBoxAktif";
+			this.comboBoxAktif.Size = new System.Drawing.Size(180, 26);
+			this.comboBoxAktif.TabIndex = 65;
+			// 
+			// textBoxSearch
+			// 
+			this.textBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.textBoxSearch.Location = new System.Drawing.Point(95, 478);
+			this.textBoxSearch.Margin = new System.Windows.Forms.Padding(4);
+			this.textBoxSearch.Name = "textBoxSearch";
+			this.textBoxSearch.Size = new System.Drawing.Size(266, 24);
+			this.textBoxSearch.TabIndex = 64;
+			// 
+			// label6
+			// 
+			this.label6.AutoSize = true;
+			this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.label6.Location = new System.Drawing.Point(20, 481);
+			this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(67, 18);
+			this.label6.TabIndex = 63;
+			this.label6.Text = "Search  :";
+			this.label6.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// buttonClear
+			// 
+			this.buttonClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.buttonClear.Location = new System.Drawing.Point(1066, 604);
+			this.buttonClear.Margin = new System.Windows.Forms.Padding(4);
+			this.buttonClear.Name = "buttonClear";
+			this.buttonClear.Size = new System.Drawing.Size(116, 36);
+			this.buttonClear.TabIndex = 62;
+			this.buttonClear.Text = "Clear";
+			this.buttonClear.UseVisualStyleBackColor = true;
+			// 
+			// buttonDelete
+			// 
+			this.buttonDelete.Enabled = false;
+			this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.buttonDelete.Location = new System.Drawing.Point(1066, 560);
+			this.buttonDelete.Margin = new System.Windows.Forms.Padding(4);
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.Size = new System.Drawing.Size(116, 36);
+			this.buttonDelete.TabIndex = 61;
+			this.buttonDelete.Text = "Delete";
+			this.buttonDelete.UseVisualStyleBackColor = true;
+			// 
+			// buttonUpdate
+			// 
+			this.buttonUpdate.Enabled = false;
+			this.buttonUpdate.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.buttonUpdate.Location = new System.Drawing.Point(1066, 516);
+			this.buttonUpdate.Margin = new System.Windows.Forms.Padding(4);
+			this.buttonUpdate.Name = "buttonUpdate";
+			this.buttonUpdate.Size = new System.Drawing.Size(116, 36);
+			this.buttonUpdate.TabIndex = 60;
+			this.buttonUpdate.Text = "Update";
+			this.buttonUpdate.UseVisualStyleBackColor = true;
+			// 
+			// btnInsert
+			// 
+			this.btnInsert.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.btnInsert.Location = new System.Drawing.Point(1066, 472);
+			this.btnInsert.Margin = new System.Windows.Forms.Padding(4);
+			this.btnInsert.Name = "btnInsert";
+			this.btnInsert.Size = new System.Drawing.Size(116, 36);
+			this.btnInsert.TabIndex = 59;
+			this.btnInsert.Text = "Insert";
+			this.btnInsert.UseVisualStyleBackColor = true;
+			// 
+			// textBoxUsername
+			// 
+			this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.textBoxUsername.Location = new System.Drawing.Point(118, 556);
+			this.textBoxUsername.Margin = new System.Windows.Forms.Padding(4);
+			this.textBoxUsername.Name = "textBoxUsername";
+			this.textBoxUsername.Size = new System.Drawing.Size(266, 24);
+			this.textBoxUsername.TabIndex = 58;
+			// 
+			// textBoxId
+			// 
+			this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.textBoxId.Location = new System.Drawing.Point(118, 524);
+			this.textBoxId.Margin = new System.Windows.Forms.Padding(4);
+			this.textBoxId.Name = "textBoxId";
+			this.textBoxId.ReadOnly = true;
+			this.textBoxId.Size = new System.Drawing.Size(110, 24);
+			this.textBoxId.TabIndex = 57;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.label2.Location = new System.Drawing.Point(21, 556);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(89, 18);
+			this.label2.TabIndex = 56;
+			this.label2.Text = "Username  :";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+			this.label1.Location = new System.Drawing.Point(15, 525);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(94, 18);
+			this.label1.TabIndex = 55;
+			this.label1.Text = "ID Category :";
+			this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+			// 
+			// dataGridViewUser
+			// 
+			this.dataGridViewUser.AllowUserToAddRows = false;
+			this.dataGridViewUser.AllowUserToDeleteRows = false;
+			this.dataGridViewUser.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+			this.dataGridViewUser.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewUser.Location = new System.Drawing.Point(18, 61);
+			this.dataGridViewUser.Margin = new System.Windows.Forms.Padding(4);
+			this.dataGridViewUser.Name = "dataGridViewUser";
+			this.dataGridViewUser.ReadOnly = true;
+			this.dataGridViewUser.RowHeadersVisible = false;
+			this.dataGridViewUser.Size = new System.Drawing.Size(1164, 403);
+			this.dataGridViewUser.TabIndex = 54;
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10F);
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.kembaliKeMenuToolStripMenuItem,
+            this.masterTransaksiToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Padding = new System.Windows.Forms.Padding(9, 3, 0, 3);
+			this.menuStrip1.Size = new System.Drawing.Size(1204, 29);
+			this.menuStrip1.TabIndex = 53;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// kembaliKeMenuToolStripMenuItem
+			// 
+			this.kembaliKeMenuToolStripMenuItem.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+			this.kembaliKeMenuToolStripMenuItem.Name = "kembaliKeMenuToolStripMenuItem";
+			this.kembaliKeMenuToolStripMenuItem.Size = new System.Drawing.Size(127, 23);
+			this.kembaliKeMenuToolStripMenuItem.Text = "Kembali ke Menu";
+			this.kembaliKeMenuToolStripMenuItem.Click += new System.EventHandler(this.kembaliKeMenuToolStripMenuItem_Click);
+			// 
+			// masterTransaksiToolStripMenuItem
+			// 
+			this.masterTransaksiToolStripMenuItem.Name = "masterTransaksiToolStripMenuItem";
+			this.masterTransaksiToolStripMenuItem.Size = new System.Drawing.Size(12, 23);
+			// 
+			// FormCategory
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1204, 649);
+			this.Controls.Add(this.buttonClearSearch);
+			this.Controls.Add(this.comboBoxAktif);
+			this.Controls.Add(this.textBoxSearch);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.buttonClear);
+			this.Controls.Add(this.buttonDelete);
+			this.Controls.Add(this.buttonUpdate);
+			this.Controls.Add(this.btnInsert);
+			this.Controls.Add(this.textBoxUsername);
+			this.Controls.Add(this.textBoxId);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.dataGridViewUser);
+			this.Controls.Add(this.menuStrip1);
+			this.Name = "FormCategory";
+			this.Text = "FormCategory";
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewUser)).EndInit();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button buttonClearSearch;
+		private System.Windows.Forms.ComboBox comboBoxAktif;
+		private System.Windows.Forms.TextBox textBoxSearch;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.Button buttonClear;
+		private System.Windows.Forms.Button buttonDelete;
+		private System.Windows.Forms.Button buttonUpdate;
+		private System.Windows.Forms.Button btnInsert;
+		private System.Windows.Forms.TextBox textBoxUsername;
+		private System.Windows.Forms.TextBox textBoxId;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.DataGridView dataGridViewUser;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem kembaliKeMenuToolStripMenuItem;
+		private System.Windows.Forms.ToolStripMenuItem masterTransaksiToolStripMenuItem;
+	}
+}
